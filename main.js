a45b1e61f9603a4e0d55a9a478a167e5518f11be
CrazyJannis44 <36644360+CrazyJannis44@users.noreply.github.com>
2024-04-05 21:10:08 +0000

    Changed link concatenation so you have the option to add parameters in your custom link

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ openInCyberChef = function(word){
   var query = word.selectionText;
   browser.storage.local.get("my_url", function(items) {
 	if (!!items.my_url) {
-		browser.tabs.create({url: items.my_url + "#input=" + encodeForCyberChef(query)});
+		browser.tabs.create({url: items.my_url + "#&input=" + encodeForCyberChef(query)});
 	} else {
 		browser.tabs.create({url: "https://gchq.github.io/CyberChef#input=" + encodeForCyberChef(query)});
 	}
